bcbfea017217c755ab6563786ca0cedb3303c9ed
Jose Luis Zamora <jzamora@geo.gob.bo>
2014-05-07 14:16:28 -0400

    add all scripts

--- application/views/template/header_liquidation.php
+++ application/views/template/header_liquidation.php
@@ -2,18 +2,14 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Horizon Liquidaciones</title>
 
-    <title>Horizon Liquidaciones</title>
-
-    <!-- Core CSS - Include with every page -->
-    <link href="<?php echo base_url(); ?>css/normalize.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>font-awesome/css/font-awesome.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>css/sb-admin.css" rel="stylesheet">
-
-    
+  <link href="<?php echo base_url(); ?>css/normalize.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>font-awesome/css/font-awesome.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>css/sb-admin.css" rel="stylesheet">    
 </head>
 <body>
 
