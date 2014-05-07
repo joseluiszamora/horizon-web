942864b0b9a8334d545b5195449cc67bb98c789c
Jose Luis Zamora <jzamora@geo.gob.bo>
2014-05-07 13:32:13 -0400

    fonts

--- application/views/template/footer_liquidation.php
+++ application/views/template/footer_liquidation.php
@@ -3,3 +3,11 @@
 
 </body>
 </html>
+
+<!-- Core Scripts  -->
+<script src="<?php echo base_url(); ?>js/jquery-1.11.0.min.js"></script>
+<script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
+<script src="<?php echo base_url(); ?>js/plugins/metisMenu/jquery.metisMenu.js"></script>
+<script src="<?php echo base_url(); ?>js/sb-admin.js"></script>
+<script src="<?php echo base_url(); ?>js/hogan-2.0.0.js"></script>
+<script src="<?php echo base_url(); ?>js/line_volumes.js"></script>
--- application/views/template/header_liquidation.php
+++ application/views/template/header_liquidation.php
@@ -14,14 +14,6 @@
     <link href="<?php echo base_url(); ?>css/sb-admin.css" rel="stylesheet">
 
     
-    <!-- Core Scripts  -->
-    <script src="<?php echo base_url(); ?>js/jquery-1.11.0.min.js"></script>
-    <script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
-    <script src="<?php echo base_url(); ?>js/plugins/metisMenu/jquery.metisMenu.js"></script>
-    <script src="<?php echo base_url(); ?>js/sb-admin.js"></script>
-    <script src="<?php echo base_url(); ?>js/hogan-2.0.0.js"></script>
-    <script src="<?php echo base_url(); ?>js/line_volumes.js"></script>
-
 </head>
 <body>
 
