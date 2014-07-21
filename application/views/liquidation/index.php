d6507e09435c846cb07e95f6d2152af0c06d0063
Elmer Mendoza <emendoza@geo.gob.bo>
2014-07-21 19:50:56 -0400

    Add d3.js barchart examples

--- application/views/liquidation/index.php
+++ application/views/liquidation/index.php
@@ -1,11 +1,46 @@
+<script src="http://d3js.org/d3.v3.min.js"></script>
+<script src="<?php echo base_url(); ?>js/d3js/RadarChart.js"></script>
+<style>
+
+.bar {
+  fill: steelblue;
+}
+
+.bar:hover {
+  fill: brown;
+}
+
+.axis {
+  font: 10px sans-serif;
+}
+
+.axis path,
+.axis line {
+  fill: none;
+  stroke: #000;
+  shape-rendering: crispEdges;
+}
+
+.x.axis path {
+  display: none;
+}
+
+</style>
+
 <div class="row">
   <div class="col-lg-12">
-    <h3 class="page-header">Estadisticas</h3>
+    <h3 class="page-header">Estadísticas</h3>
   </div>
 </div>
 
 <div class="row">
-  <div class="col-lg-12">
-    <h4>Proximamente</h4>
+  <div id="body">
+    <div id="chart"></div>
   </div>
+
+  <div id="barchart"></div>
 </div>
+
+
+<script src="<?php echo base_url(); ?>js/d3js/script.js"></script>
+<script src="<?php echo base_url(); ?>js/d3js/barchart.js"></script>
